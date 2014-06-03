563c05e6ce6a0ce2ad234b9f8bad00eea8e37d44
sonic1226 <alalvaro_7@hotmail.com>
2014-06-03 23:44:18 +0000

    adicionado arquivos compilados

---
+++ CMakeFiles/picamdemo.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/dtracker/dtracker/camera.cpp" "/home/pi/dtracker/dtracker/CMakeFiles/picamdemo.dir/camera.cpp.o"
+  "/home/pi/dtracker/dtracker/cameracontrol.cpp" "/home/pi/dtracker/dtracker/CMakeFiles/picamdemo.dir/cameracontrol.cpp.o"
+  "/home/pi/dtracker/dtracker/graphics.cpp" "/home/pi/dtracker/dtracker/CMakeFiles/picamdemo.dir/graphics.cpp.o"
+  "/home/pi/dtracker/dtracker/picam.cpp" "/home/pi/dtracker/dtracker/CMakeFiles/picamdemo.dir/picam.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/opt/vc/include"
+  "/opt/vc/userland-master/interface/vcos"
+  "/opt/vc/userland-master"
+  "/opt/vc/userland-master/interface/vcos/pthreads"
+  "/opt/vc/userland-master/interface/vmcs_host/linux"
+  "/opt/vc/userland-master/host_applications/linux/libs/bcm_host/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
